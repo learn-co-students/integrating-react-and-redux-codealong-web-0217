ca9e688f6ac9b4df343ff49628f33b7c92ca6d6d
github username <github email address>
2017-04-12 17:35:17 -0400

    done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export default (props) => {
-  return (
-    <div>
-      Counter Component
-    </div>
-  )
-};
+  export default (props) => {
+
+    const handleOnClick = () => {
+      props.store.dispatch({ type: 'INCREASE_COUNT' });
+    }
+
+    return (
+      <div>
+        <button onClick={handleOnClick}>
+          Click Me
+        </button>
+        <div>{props.store.getState().count}</div>
+      </div>
+    )
+  };
+
+
+
+// import React from 'react'
+//
+// export default (props) => {
+//   return (
+//    <div>
+//      <button>Click Me</button>
+//      <div>0</div>
+//    </div>
+//  )
+// };
