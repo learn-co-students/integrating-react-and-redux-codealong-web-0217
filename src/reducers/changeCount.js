1adfa4f8e771f379785aba111b1e4637d1d53d7b
Daniel <daniel.seehausen@gmail.com>
2017-04-12 10:49:00 -0400

    Done.

---
+++ src/reducers/changeCount.js
@@ -0,0 +1,13 @@
+// ./src/reducers/changeCount.js
+
+export default function changeCount(state = {
+  count: 0
+}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+
+    default:
+      return state;
+  };
+};
